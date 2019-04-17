282090b83a4aa4f1170733111d7b8e521fd1f969
g_that_boul <g_that_boul@Admins-MacBook-Air.local>
2019-04-17 08:52:42 -0400

    OOP

--- Class-Code/OOP/object-classes/ghouston/sketch.js
+++ Class-Code/OOP/object-classes/ghouston/sketch.js
@@ -1,6 +1,6 @@
-//Your Name(s)
-//Project Name
-//Date
+//Gordon Houston III
+//OOP
+//4/17/19
 
 /* A brief description of what the program is */
 
@@ -8,46 +8,30 @@
 let x, y, r;
 let xVel = 5;
 let yVel = 5;
-let bubble;
+let bubble0;
+let bubble1;
 
 function setup() {
     createCanvas(600, 400);
-    // x = width / 2;
-    // y = height / 2;
-    // r = 10;
-
-    bubble = new Bubble()
-
+    bubble0 = new Bubble()
+    bubble1 = new Bubble()
     ellipseMode(RADIUS)
 }
 
 function draw() {
     background(0, 50);
-    bubble.move()
-    bubble.display()
+    bubble0.move()
+    bubble0.display()
+    bubble0.checkEdges()
+
+    bubble1.move()
+    bubble1.display()
+    bubble1.checkEdges()
 }
-// moveBy();
-// show();
-
-//display
-// fill(0, 255, 100);
-// stroke(255);
-// ellipse(x, y, r)
-
-// //move ball
-
-// x += xVel;
-// y += yVel;
-
-checkEdges
-
-if (x + r >= width || x - r <= 0) xVel *= -1;
-if (y + r >= height || y - r <= 0) yVel *= -1;
-
 class Bubble {
     constructor() {
-        this.x = 5
-        this.y = 5
+        this.x = random(10, width - 10)
+        this.y = random(10, height - 10)
         this.r = 10
         this.xVel = 5
         this.yVel = 5
@@ -69,8 +53,3 @@ class Bubble {
         this.y += this.yVel;
     }
 }
-// bubble = new Bubble
-//     constructor()
-//     this.x = 10
-//     this.y = 5
-//     this.r = 5
